f820bdfb5cea4bb372a34d18020db89cba95f532
Bartek <Bartek@DESKTOP-QRR7V76>
2021-01-11 11:23:54 +0100

    Poprawki

--- Tescik/test/tescik/TescikTest.java
+++ Tescik/test/tescik/TescikTest.java
@@ -51,7 +51,7 @@ public class TescikTest {
     public void testMain() {
         System.out.println("main");
         String[] args = null;
-      //  Tescik.main(args);
+        Tescik.main(args);
         // TODO review the generated test code and remove the default call to fail.
         fail("Wygenerowano perfidny błąd!");
     }
